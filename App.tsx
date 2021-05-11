5de8bce9a0f5cc7a01bb66b23360f7cc758a36c2
Gabriel Cunha <gn_cunha@hotmail.com>
2021-05-10 21:48:13 -0300

    adicionado splashScreen e ícone

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Provider} from 'react-redux';
 
 import dados from './src/store/store';
+import SplashScreen from 'react-native-splash-screen';
 import Routes from './routes';
-
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={dados}>
       <Routes />
